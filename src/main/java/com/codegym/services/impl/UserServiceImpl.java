bdc32f66ebdc15836a88211ebd2351f120448fb9
Nguyen Khac Gia Bao <Giabaoproo347@gmail.com>
2020-02-10 23:11:20 +0700

    Thành sửa

--- src/main/java/com/codegym/services/impl/UserServiceImpl.java
+++ src/main/java/com/codegym/services/impl/UserServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 @Service
-public class UserImpl implements UserService {
+public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
 
